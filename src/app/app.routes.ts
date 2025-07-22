203d50b514d7401047da6aaf3ffc2195774ffd20
M2M2005 <bonscyprien4@gmail.com>
2025-07-22 16:59:00 +0200

    nouvelle page d'accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home }, // Cette ligne définit ta page d'accueil
+  { path: 'game', loadComponent: () => import('./game/game').then(m => m.GameComponent)}, // J'ai une erreur ici, car tu n'as pas de GameComponent dans game.ts
+  { path: '**', redirectTo: '' }
+];
